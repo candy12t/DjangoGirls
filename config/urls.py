612a3db0180bb0b38491b2f29d137fbc282f0e8f
candy12t <candy12t@gmail.com>
2019-11-08 00:22:43 +0900

    ログインurl作成

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('', include('blog.urls')),
 ]
