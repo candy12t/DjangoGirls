31cf3ec0b04225e904cbc174aa6cbe1b82f1e668
candy12t <candy12t@gmail.com>
2019-11-08 11:30:27 +0900

    修正&マイグレーション

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-  post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comment')
+  post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
   author = models.CharField(max_length=200)
   text = models.TextField()
   created_date = models.DateTimeField(default=timezone.now)
